2aedecb375b2443a96d11554368fa3aca01994a9
Filipe Cabaco <filipecabaco@gmail.com>
2014-12-06 20:58:00 +0000

    Cleaned more stuff

--- backend/db_module/DbModule.py
+++ backend/db_module/DbModule.py
@@ -9,7 +9,7 @@ class DbModule():
     def start(self):
         mongoengine.connect('TweetInTime',host=config.get('System', 'mongo'))
     def create_tweet(self,tweetJson):
-        tweet = self.getTweet(tweetJson.id_str)
+        tweet = self.get_tweet(tweetJson.id_str)
         if not tweet:
             tweet = Tweet()
         for attr in tweet._fields.keys():
@@ -21,16 +21,11 @@ class DbModule():
         tweet =  Tweet.objects(id_str=id_str)
         return tweet[0] if tweet else None
 
-    def exists_tweet(self,id_str):
-        return bool(self.getTweet(id_str))
-
-
 if __name__ == '__main__':
 	config = ConfigParser.ConfigParser()
 	config.read('../../config.cfg')
-
 dbModule = DbModule()
 dbModule.start()
 print dbModule.get_tweet("test")
 dbModule.create_tweet(Tweet(id_str="test", name="test",text="aojfdajfalskjdlasjdlksjd",loc="",sentiment=0,created_at=datetime.datetime.now()))
-print dbModule.exists_tweet("test")
+print dbModule.get_tweet("test")
