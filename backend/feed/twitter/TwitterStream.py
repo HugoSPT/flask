ea3932dd99dcbc68933d14049a6769426c60613f
Fábio Botelho <fabio.botelho@seedcloud.com>
2014-12-06 18:29:36 +0000

    Added rabbit mq producer example

--- backend/feed/twitter/TwitterStream.py
+++ backend/feed/twitter/TwitterStream.py
@@ -1,14 +1,25 @@
+import ConfigParser
+import pickle
+
 __author__ = 'fabiim'
 
 import time
 import random
 
+config = ConfigParser.ConfigParser()
+config.read('../../../config.cfg')
+
+import backend.feed.util.QueueHandler as QueueHandler
 from twython import TwythonStreamer
+
+RABBIT_MQ_IP = config.get("System", "rabbit")
+QUEUE_FEED = config.get("Queue", "feed")
 
 def search_words():
     with open("./resources/EffectWordNet.tff") as f:
-        def split_line(x)   :
+        def split_line(x):
             return x.split("\t")
+
         effects_and_words = [(split_line(x)[1], split_line(x)[2].split(",")) for x in f.readlines()]
 
         positives = []
@@ -26,12 +37,15 @@ class MyStreamer(TwythonStreamer):
     def __init__(self, app_key, app_secret, oauth_token, oauth_token_secret, timeout=300, retry_count=None, retry_in=10,
                  client_args=None, handlers=None, chunk_size=1):
         super(MyStreamer, self).__init__(app_key, app_secret, oauth_token, oauth_token_secret, timeout, retry_count,
-                                         retry_in, client_args, handlers, 1000)
+                                         retry_in, client_args, handlers, chunk_size)
         self._n = 0
+        self.queue_handler = QueueHandler.RabbitQueueHandler()
+        self.publisher = self.queue_handler.pub_register(QUEUE_FEED)
 
     def on_success(self, data):
         if 'text' in data:
-            print data['text'].encode('utf-8')
+            print(data)
+            self.publisher.publish_task(data=pickle.dumps(data), keys=QUEUE_FEED)
 
     def on_error(self, status_code, data):
         print "Error code: " + str(status_code) + " - " + str(data)
@@ -40,10 +54,18 @@ class MyStreamer(TwythonStreamer):
             self._n += 1
 
 
+def run_feed_listener(trace_words):
+        stream = MyStreamer("vYn0ES5AIqaxtl582gcQrfozZ", "S22Tah9Plo4vlJzQ1WgZYt4ML8FoacQ9QMTa1KjvHiZTs3DkMF",
+                        "296786432-hI6eoypFeXRYjpKzSo1QMsgoVh1iNC8Qmn2Bgya0",
+                        "3vcmBuleaxirMfU3amLDGAIUUQDT0CutAtDoM1De8Xm8t")
+
+        stream.statuses.filter(lang="en", track=trace_words)
+
 def main():
     positives, negatives = search_words()
 
     def word_size(x): return len(x) + 1000 if "_" in x else 0
+
     positives = sorted(positives, lambda x, y: word_size(x) - word_size(y))
     negatives = sorted(negatives, lambda x, y: word_size(x) - word_size(y))
 
@@ -51,11 +73,16 @@ def main():
 
     trace_words = ",".join(positives) + ",".join(negatives)
 
-    stream = MyStreamer("vYn0ES5AIqaxtl582gcQrfozZ", "S22Tah9Plo4vlJzQ1WgZYt4ML8FoacQ9QMTa1KjvHiZTs3DkMF",
-                        "296786432-hI6eoypFeXRYjpKzSo1QMsgoVh1iNC8Qmn2Bgya0",
-                        "3vcmBuleaxirMfU3amLDGAIUUQDT0CutAtDoM1De8Xm8t")
+    run_feed_listener(trace_words)
 
-    stream.statuses.filter(lang="en", track=trace_words)
+
+        # except Exception:
+        #     continue
+
+    # auth = OAuthHandler(ckey, csecret)
+    # auth.set_access_token(atoken, asecret)
+    # twitterStream = Stream(auth, listener())
+    # twitterStream.filter(track=trace_words.split(", "))
 
 
 if __name__ == '__main__':
